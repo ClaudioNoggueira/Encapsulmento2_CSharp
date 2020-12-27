2ca03b3cfada5769a79ef5f2d3c69603eef50442
Claudio Nogueira <cllaudionoggueira@gmail.com>
2020-12-27 15:35:12 -0300

    First commit

---
+++ Encapsulamento2_CSharp/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace Encapsulamento2_CSharp {
+    class Produto {
+        private string nome;
+        public float preco { get; set; }
+
+        public int qtde { get; set; }
+        public string Nome {
+            get => nome;
+            set {
+                if (value != null) {
+
+                }
+            }
+        }
+
+        public Produto(string nome, float preco, int qtde) {
+            this.nome = nome;
+            this.preco = preco;
+            this.qtde = qtde;
+        }
+
+        public float valorTotalEmEstoque() {
+            return this.preco * this.qtde;
+        }
+
+        public void adicionarProdutos(int qtde) {
+            this.qtde += qtde;
+        }
+
+        public void removerProdutos(int qtde) {
+            this.qtde -= qtde;
+        }
+
+        public override string ToString() {
+            return this.nome + ", R$ " + this.preco.ToString("F2", CultureInfo.InvariantCulture).Replace(".", ",")
+         
+                + ", " + this.qtde + " unidades, Total: R$ " + valorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture).Replace(".", ",");
+        }
+    }
+}
---
+++ Encapsulamento2_CSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento2_CSharp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com os dados do produto");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            float preco = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto produto = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + produto);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            produto.adicionarProdutos(qtde);
+
+            Console.WriteLine("Produtos atualizados: " + produto);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o número de produtos a ser removida do estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            produto.removerProdutos(qtde);
+
+            Console.WriteLine("Produtos atualizados: " + produto);
+        }
+    }
+}
